947f4bd6224234f0225cd8fe5b2b04e89d938f67
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2025-11-22 16:13:45 +0200

    image squarer now pixel perfect.

--- utils/image_squarer.py
+++ utils/image_squarer.py
@@ -2,26 +2,22 @@ from PIL import Image
 
 
 def crop_to_square(image_path, output_path):
-	# Open the image
+
 	img = Image.open(image_path)
 	width, height = img.size
 
-	# Determine the size of the square
 	new_size = min(width, height)
 
-	# Calculate cropping coordinates to center the square
-	left = (width - new_size) / 2
-	top = (height - new_size) / 2
+	left = (width - new_size) // 2
+	top = (height - new_size) // 2
 	right = left + new_size
 	bottom = top + new_size
 
-	# Crop the image
 	img_cropped = img.crop((left, top, right, bottom))
 
-	# Save the cropped image
 	img_cropped.save(output_path)
 	print(f"Cropped image saved to {output_path}")
 
 
 if __name__ == '__main__':
-	crop_to_square("input.jpg", "output.jpg")
+	crop_to_square("../original_images/cat.webp", "../squared_images/output.webp")
